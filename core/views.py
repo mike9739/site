08ccdc763cea7d69f79183a9d43d006f84f09cb0
Miguel Ángel Vázquez Ulloa <migu_el_39@hotmail.com>
2019-05-12 21:41:58 -0500

    templates creadas

--- core/views.py
+++ core/views.py
@@ -1,23 +1,16 @@
 from django.shortcuts import render,HttpResponse
 html_base = """
-<h1>WEB</h1>
-<ul>
-    <li><a href="/">Portada</a></li>
-    <li><a href="/about">Acerca de</a></li>
-    <li><a href="/portafolio">Portafolio</a></li>
-    <li><a href="/contact">Contacto</a></li>
-    
-</ul>
+
 """
 # Create your views here.
 def home(request):
-    return HttpResponse( html_base + "<h2>home</h2>")
+    return render(request,"core/home.html")
 
 def about(request):
-    return HttpResponse(html_base + "<h2>about</h2><p>Pasteleria</p>")
+    return render(request,"core/about.html")
 
 def portafolio(request):
-    return HttpResponse(html_base + "<h2>portafolio</h2><p>trabajos</p>")
+    return render(request,"core/portafolio.html")
 
 def contacto(request):
-    return HttpResponse(html_base + "<h2>contacto</h2>")
+    return render(request,"core/contacto.html")
